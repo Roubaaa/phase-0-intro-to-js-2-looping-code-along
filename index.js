c813e5ce71b4f9c465292bb16fbd7467c86badba
Ruba Alsoheil <rubasoheil@gmail.com>
2022-11-03 14:30:55 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solutions in this file
+/*let spy;
+beforeEach( () => {
+    spy = sinon.spy( console, 'log' );
+  } );
+
+  afterEach( () => {
+    spy.restore();
+  } );*/
+
+
+function countDown (positiveInteger){
+let i = 0;
+    while (i <= positiveInteger){
+        console.log(positiveInteger -i)
+        positiveInteger --;
+    }
+
+}
+countDown(11);
+
+
+function writeCards(stringNames, eventName) {
+
+    let n = stringNames.length;
+    let result =[];
+   
+
+      
+      for (let i = 0; i<n;i++){
+      
+     result[i] =`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`
+     
+    
+    }
+  
+    return [...result];
+ 
+  }
+  
+console.log(writeCards( ["Guadalupe", "Ollie", "Aki"],"surprise"));
+  
